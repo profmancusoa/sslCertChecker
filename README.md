5837b70339564b2fe2d36ac8cab0299b8956db5d
profmancusoa <profmancusoa@gmail.com>
2025-04-14 16:31:21 +0200

    - initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sslCertChecker
+
 Simple SSL certificate validity checker
+
+Recently Let's encrypt (March 2025) announced that will not anymore notify users for certificate expiration.
+
+This inspired this simple but useful tool
---
+++ sslCertChecker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Author: Antonio Mancuso
+# Date: 14 Apr 2025
+#
+# Quick SSL cert expiration checker
+#
+
+
+domains=(
+    "your.domain.com" 
+    "another.domanin.com" 
+)
+
+len=${#domains[@]}
+seperator=-------------------------
+rows="%-25s| %-25s\n"
+
+
+getSSLExpire() {
+    # $1: ssl domain to check
+    res=$(openssl s_client -showcerts -connect $1:443 <<< "Q" 2>/dev/null | openssl x509 -text | grep "Not After")
+    echo $res | awk -F " : " '{print $2}'
+}
+
+tableHEder() {
+    #print header
+    printf "\n$rows" "Certificate" "Expires On"
+    printf "$rows" "$seperator" "$seperator"
+}
+
+tableBody() {
+    #print lines
+    for((i=0; i < len; i++)); do
+        expiry=$(getSSLExpire ${domains[$i]})
+        printf "$rows" ${domains[$i]} "$expiry"
+    done
+    printf "\n"
+}
+
+tableHEder
+tableBody
